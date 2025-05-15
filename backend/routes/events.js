0abd4e345f1df85ec7354a58b879261e017d52d5
Alozi <alozimozi@gmail.com>
2025-05-15 17:05:45 +0300

    Outputed validation errors from backend

--- backend/routes/events.js
+++ backend/routes/events.js
@@ -60,10 +60,10 @@ router.post('/', async (req, res, next) => {
 
   try {
     await add(data);
-    setTimeout(() => {
-      res.status(201).json({ message: 'Event saved.', event: data });
-    }, 1500);
-    // res.status(201).json({ message: 'Event saved.', event: data });
+    // setTimeout(() => {
+    //   res.status(201).json({ message: 'Event saved.', event: data });
+    // }, 1500);
+    res.status(201).json({ message: 'Event saved.', event: data });
   } catch (error) {
     next(error);
   }
