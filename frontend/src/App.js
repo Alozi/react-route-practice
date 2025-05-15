857b79a830e3f364b8e75bababcd478a004df5da
Alozi <alozimozi@gmail.com>
2025-05-15 17:24:25 +0300

    Create one common action for pages EditEvent and NewEvent

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -26,10 +26,11 @@ import Root from './pages/Root';
 import Home from './pages/Home';
 import Events, { loader as eventsLoader } from './pages/Events';
 import EventDetail, { loader as eventDetailLoader, action as deleteEventAction } from './pages/EventDetail';
-import NewEvent, { action as newEventAction } from './pages/NewEvent';
+import NewEvent from './pages/NewEvent';
 import EditEvent from './pages/EditEvent';
 import EventRoot from './pages/EventRoot';
 import Error from './pages/Error';
+import { action as manipulateEventAction } from './components/EventForm';
 
 const router = createBrowserRouter([{
   path: '/',
@@ -48,10 +49,10 @@ const router = createBrowserRouter([{
           loader: eventDetailLoader,
           children: [
             { index: true, element: <EventDetail />, action: deleteEventAction },
-            { path: 'edit', element: <EditEvent /> },
+            { path: 'edit', element: <EditEvent />, action: manipulateEventAction },
           ]
         },
-        { path: 'new', element: <NewEvent />, action: newEventAction }
+        { path: 'new', element: <NewEvent />, action: manipulateEventAction }
       ]
     },
   ],
