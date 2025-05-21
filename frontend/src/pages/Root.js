4bdb57bb3a07f00c04a434e4972cd213f962ff24
Alozi <alozimozi@gmail.com>
2025-05-21 16:17:55 +0300

    Add automatic logout

--- frontend/src/pages/Root.js
+++ frontend/src/pages/Root.js
@@ -1,9 +1,31 @@
-import { Outlet } from 'react-router-dom';
+import { Outlet, useLoaderData, useSubmit } from 'react-router-dom';
 
 import MainNavigation from '../components/MainNavigation';
+import { useEffect } from 'react';
+import { getTokenDuration } from '../util/auth';
 
 export default function Root() {
     // const navigation = useNavigation();
+    const token = useLoaderData();
+    const submit = useSubmit();
+
+    useEffect(() => {
+        if (!token) {
+            return;
+        }
+
+        if (token === 'EXPIRED') {
+            submit(null, { action: '/logout', method: 'post' });
+        }
+
+        const tokentDuration = getTokenDuration();
+        console.log(tokentDuration);
+
+        setTimeout(() => {
+            submit(null, { action: '/logout', method: 'post' })
+        }, tokentDuration);
+
+    }, [token, submit]);
 
     return <>
         <MainNavigation />
